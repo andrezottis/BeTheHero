fc48058168a0a9607e56eb1d4bad8d6b96c3d8d7
André Zottis <zottis.andre@outlook.com>
2020-03-26 19:34:59 -0300

    Created register and profile pages

--- frontend/src/pages/logon/index.js
+++ frontend/src/pages/logon/index.js
@@ -1,30 +1,34 @@
-import React from 'react';
-import {FiLogIn} from 'react-icons/fi'
+import React from "react";
+import {Link} from "react-router-dom";
 
-import './styles.css';
+import { FiLogIn } from "react-icons/fi";
 
-import heroesImg from '../../assets/heroes.png';
-import logoImg from '../../assets/logo.svg';
+import "./styles.css";
 
-export default function Logon (){
-    return (
-        <div className="logon-container">
-            <section className="form">
-                <img src= {logoImg} alt="Be The Hero" />
+import heroesImg from "../../assets/heroes.png";
+import logoImg from "../../assets/logo.svg";
 
-                <form>
-                    <h1> Faça seu logon</h1>
+export default function Logon() {
+  return (
+    <div className="logon-container">
+      <section className="form">
+        <img src={logoImg} alt="Be The Hero" />
 
-                    <input placeholder="Sua ID" />
-                    <button className="button" type="submit">Entrar</button>
+        <form>
+          <h1> Faça seu logon</h1>
 
-                    <a href="./register">
-                        <FiLogIn size={16} color="#E02041"/>
-                        Não tenho cadastro
-                        </a>
-                </form>
-            </section>
-            <img src={heroesImg} alt="Heroes" />
-        </div>
-    );
+          <input placeholder="Sua ID" />
+          <button className="button" type="submit">
+            Entrar
+          </button>
+
+          <Link className="back-link" to="./register">
+            <FiLogIn size={16} color="#E02041" />
+            Não tenho cadastro
+          </Link>
+        </form>
+      </section>
+      <img src={heroesImg} alt="Heroes" />
+    </div>
+  );
 }
