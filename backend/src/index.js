13bdd4085507486a88bd63c9bdf15feb17f5b29c
André Zottis <zottis.andre@outlook.com>
2020-03-25 21:15:28 -0300

    Added controllers and finished database.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const routes = require('./routes');
+const cors = require('cors');
+
 const app = express();
 
+app.use(cors()); //leave empty for development
 //Inform to JS api that the body requests will be received in JSON format.
 app.use(express.json());
 app.use(routes);
-
 
 app.listen(3333);
