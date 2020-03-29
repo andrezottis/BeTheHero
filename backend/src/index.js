42996528b5c643b8c9f1e5bc7f2774b0a648824c
André Zottis <zottis.andre@outlook.com>
2020-03-29 12:59:29 -0300

    added backend validation

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 const cors = require('cors');
+const {errors} = require('celebrate');
 
 const app = express();
 
@@ -8,5 +9,6 @@ app.use(cors()); //leave empty for development
 //Inform to JS api that the body requests will be received in JSON format.
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
 app.listen(3333);
